d7e649f963be44cb6cd1bfda2e319bc5a01ea3a9
Rien Maertens <rien.maertens@posteo.be>
2021-07-20 00:39:57 +0200

    Fix nixosModule and nixosModules in flake

--- flake.nix
+++ flake.nix
@@ -1,6 +1,7 @@
 {
   description = "A modern music server focusing on metadata";
-  outputs = { self, nixpkgs }: {
-    nixosModule = import ./default.nix;
+  outputs = { self, nixpkgs }: rec {
+    nixosModules.accentor = import ./default.nix;
+    nixosModule = nixosModules.accentor;
   };
 }
