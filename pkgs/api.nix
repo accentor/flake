87b2e98f601d6b539682c1470dde7b38d2a349ac
Charlotte Van Petegem <charlotte@vanpetegem.me>
2022-02-12 10:43:27 +0100

    accentor-api: 0.17.0 -> 0.17.1

--- pkgs/api.nix
+++ pkgs/api.nix
@@ -10,13 +10,13 @@
 
 stdenv.mkDerivation rec {
   pname = "accentor-api";
-  version = "0.17.0";
+  version = "0.17.1";
 
   src = fetchFromGitHub {
     owner = "accentor";
     repo = "api";
     rev = "v${version}";
-    sha256 = "r4HxPlg6DCYDhzC8hCpx3UaAG5/8P5JTHRd/C5X1924=";
+    sha256 = "WPbAUZg62otClZm48sThntzQpL6AEU0G4VN8i1ov6f4=";
   };
 
   installPhase = ''
